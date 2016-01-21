29b603de238478e8f9e55a1815dc428afff6c0bd
GabrielMassana <gabrielmassana@gmail.com>
2016-01-21 17:30:30 +0000

    example

--- CoreNetworking/CoreNetworking/Example/CNMViewController.m
+++ CoreNetworking/CoreNetworking/Example/CNMViewController.m
@@ -14,14 +14,11 @@
 
 @implementation CNMViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    //http://exampleapi.com/json
 }
 
 @end
